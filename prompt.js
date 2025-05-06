75b4b4dc0b53ea04cd78e2ce531c733fb1023c3b
SousaPatricia25 <patricia.sousa2413@outlook.com>
2025-05-06 16:25:27 -0300

    Atividades

---
+++ prompt.js
@@ -0,0 +1,5 @@
+let nome = prompt("Digite seu nome");
+let profissao = prompt("Digite sua profissão");
+
+
+console.log(alert(`Ola, ${nome} Que interessante saber que voce trabalha com ${profissao}`));
